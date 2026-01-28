eadd421c1945208dcf3575011a1714c7f08374ad
Dinh Cong An <andche186895@fpt.edu.vn>
2026-01-28 16:59:24 +0700

    Add backend Spring Boot and frontend React projects

---
+++ backend/MiniMart/src/test/java/com/minimart/backend/MiniMartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minimart.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniMartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
